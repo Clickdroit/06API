de8bf39428eee9830abf4b057ec72439edca3385
Clickdroit <1erfr2007@gmail.com>
2026-03-31 21:45:15 +0200

    refactor: fix API routing logic, update table styling, and improve AJAX response rendering

--- ajax.js
+++ ajax.js
@@ -21,19 +21,16 @@ function AfficherListeHTML(){
         if (this.readyState == 4 && this.status == 200) {
             var reponse = this.responseText;
             var reponseListe = JSON.parse(reponse);
-            console.log(reponse);
             var section = document.getElementById("section");
-            //section.innerHTML = "";
-            console.log(reponseListe);
-            for(var id in reponseListe){
-                var item = reponseListe[id];
+            section.innerHTML = "";
+            for(var i = 0; i < reponseListe.length; i++){
+                var item = reponseListe[i];
                 var nom = item.nom;
                 var description = item.description;
                 var numero = item.numero;
                 var type = item.type;
-                console.log("ID: "+id+" Nom: "+nom+" Description: "+description+" Numero: "+numero+" Type: "+type);
+                console.log("ID: "+i+" Nom: "+nom+" Description: "+description+" Numero: "+numero+" Type: "+type);
                 section.innerHTML += "<div class='capteur-table'><table><tr><th>Nom</th><th>Description</th><th>Numéro</th><th>Type</th></tr><tr><td>"+nom+"</td><td>"+description+"</td><td>"+numero+"</td><td>"+type+"</td></tr></table></div>";
-                
             }
         }
     };
--- rest.php
+++ rest.php
@@ -5,62 +5,55 @@ try {
     echo("ERREUR DB \r\n");
 }
 $req_methode = $_SERVER['REQUEST_METHOD'];
-if(isset($_SERVER['PATH_INFO'])){
-    $req_path=$_SERVER['PATH_INFO'];
-    $req_data=explode('/',$req_path);
-}
+$req_data = [];
 
+if (isset($_SERVER['PATH_INFO'])) {
+    $req_path = $_SERVER['PATH_INFO'];
+    $req_data = explode('/', $req_path);
+}
 
 if ($req_methode == 'GET') {
     if (count($req_data) == 2 && $req_data[1] == 'capteur') {
-        $requete= "SELECT * FROM capteur";
+        $requete = "SELECT * FROM capteur";
         $req_prep = $maConnexion->prepare($requete);
-        $req_prep->execute(NULL);
+        $req_prep->execute();
         $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($resultat);
     }
 
     if (count($req_data) == 3 && $req_data[1] == 'capteur' && $req_data[2] == 'etat') {
-        $requete= "SELECT * FROM etat";
+        $requete = "SELECT * FROM etat";
         $req_prep = $maConnexion->prepare($requete);
-        $req_prep->execute(NULL);
+        $req_prep->execute();
         $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
-        $json_data=json_encode($resultat);
-        print_r($json_data);
+        echo json_encode($resultat);
+    }
+
+    if (count($req_data) == 4 && $req_data[1] == 'capteur' && $req_data[2] == 'etat') {
+        $id = (int)$req_data[3];
+        $requete = "SELECT * FROM etat WHERE id_capteur = :id";
+        $req_prep = $maConnexion->prepare($requete);
+        $req_prep->bindValue(':id', $id, PDO::PARAM_INT);
+        $req_prep->execute();
+        $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultat);
     }
 
     if (count($req_data) == 3 && $req_data[1] == 'capteur' && $req_data[2] == 'configuration') {
-        $requete= 'SELECT * FROM configuration';
+        $requete = "SELECT * FROM configuration";
         $req_prep = $maConnexion->prepare($requete);
-        $req_prep->execute(NULL);
-        $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
-        $json_data=json_encode($resultat);
-        print_r($json_data);
-    }
-
-    if (count($req_data) == 3 && $req_data[1] == 'capteur' && $req_data[2] == 'etat') {
-        $numero = (int)$req_data[3];
-        $requete = "SELECT * FROM etat WHERE id_capteur = ( SELECT id FROM capteur WHERE id = :id )";
-        $req_prep = $maConnexion->prepare($requete);
-        $req_prep->bindValue(':id', $numero, PDO::PARAM_INT);
         $req_prep->execute();
         $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
-        $json_data=json_encode($resultat);
-        print_r($json_data);
+        echo json_encode($resultat);
     }
-
-    if (count($req_data) == 3 && $req_data[1] == 'capteur' && $req_data[2] == 'configuration') {
-        $numero = (int)$req_data[3];
-        $requete = "SELECT * FROM configuration WHERE id_capteur IN ( SELECT id FROM capteur WHERE id = :id)";
+    if (count($req_data) == 4 && $req_data[1] == 'capteur' && $req_data[2] == 'configuration') {
+        $id = (int)$req_data[3];
+        $requete = "SELECT * FROM configuration WHERE id_capteur = :id";
         $req_prep = $maConnexion->prepare($requete);
-        $req_prep->bindValue(':id', $numero, PDO::PARAM_INT);
+        $req_prep->bindValue(':id', $id, PDO::PARAM_INT);
         $req_prep->execute();
         $resultat = $req_prep->fetchAll(PDO::FETCH_ASSOC);
-        $json_data=json_encode($resultat);
-        print_r($json_data);
-      
+        echo json_encode($resultat);
     }
 }
 ?>
-
-Failed to load resource: net::ERR_FAILEDUnderstand this error
