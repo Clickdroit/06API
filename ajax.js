e4d29e4e8ed1979b751323baf2557a1c6bda4658
Maxime <1erfr2007@gmail.com>
2026-04-02 17:00:16 +0200

    save

--- ajax.js
+++ ajax.js
@@ -7,8 +7,25 @@ function AfficherPlanHTML(){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var reponse = this.responseText;
-            var section = document.getElementById("section");
-            //section.innerHTML = reponse;
+            var reponsePlan = JSON.parse(reponse);
+            var div = document.getElementsByClassName("place");
+            for(var i = 0; i < reponsePlan.length; i++){
+                var item = reponsePlan[i];
+                var nom = item.nom;
+                var etat = item.etat;
+                console.log("ID: "+i+" Nom: "+nom+" Etat: "+etat);
+                if(etat == "Libre"){
+                    div[i].classList.add("libre");
+                    div[i].classList.remove("occupee");
+                    div = "<div id='vert' class='place'></div>"
+                }
+                else if(etat == "Occupee"){
+                    div[i].classList.add("occupee");
+                    div[i].classList.remove("libre");
+                    div = "<div id='rouge' class='place'></div>"
+                }
+                window.location.href = "plan/plan.html";
+            }
         }
     };
     xhttp.open("GET", "http://172.20.21.51/M06/rest.php/capteur/etat");
@@ -17,11 +34,13 @@ function AfficherPlanHTML(){
 
 function AfficherListeHTML(){
     var xhttp = new XMLHttpRequest();
+
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var reponse = this.responseText;
             var reponseListe = JSON.parse(reponse);
             var section = document.getElementById("section");
+            
             capteur = "<div class='capteur-table'><table><tr><th>Nom</th><th>Type</th><th>Numéro</th><th>Etat</th><th>Hauteur</th><th>Éclairage</th></tr>";
             for(var i = 0; i < reponseListe.length; i++){
                 var item = reponseListe[i];
@@ -32,8 +51,14 @@ function AfficherListeHTML(){
                 var hauteur = item.hauteur;
                 var eclairage = item.eclairage;
 
+
                 console.log("ID: "+i+" Nom: "+nom+" Type: "+type+" Numero: "+numero+" Etat: "+etat+" Hauteur: "+hauteur+" Éclairage: "+eclairage);
-                capteur = capteur + "<tbody><tr><td>"+nom+"</td><td>"+type+"</td><td>"+numero+"</td><td>"+etat+"</td><td>"+hauteur+"</td><td>"+eclairage+"</td></tr></tbody>";
+                if(etat == "Libre"){
+                    capteur = capteur + "<tbody><tr><td>"+nom+"</td><td>"+type+"</td><td>"+numero+"</td><td class='vert'>"+etat+"</td><td>"+hauteur+"</td><td>"+eclairage+"</td></tr></tbody>";
+                }
+                else if(etat == "Occupee"){
+                    capteur = capteur + "<tbody><tr><td>"+nom+"</td><td>"+type+"</td><td>"+numero+"</td><td class='rouge'>"+etat+"</td><td>"+hauteur+"</td><td>"+eclairage+"</td></tr></tbody>";
+                }
             }
             section.innerHTML = capteur + "</table></div>";
         }
